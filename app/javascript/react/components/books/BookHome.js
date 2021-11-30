6c34195f443de8d936b48dc5e101c93f7f51bc89
DragoslavR <radodrag@icloud.com>
2021-11-30 18:50:54 -0500

    added home page

---
+++ app/javascript/react/components/books/BookHome.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const BooksHomePage = () => {
+
+  return(
+    
+    <div className="home">
+      <h1>Welcome to Fantasy Book Reviews</h1>
+    </div>
+    
+  )
+}
+
+export default BooksHomePage
